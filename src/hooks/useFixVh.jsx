5a53e5a1e4217651732b94e9bc57d1170a1d7d74
Brandon Mathis <brandon@imathis.com>
2023-06-13 16:41:43 -0500

    Added logging on setVH

--- src/hooks/useFixVh.jsx
+++ src/hooks/useFixVh.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 import { useDebouncedUiCallback } from './useDebounce'
 
 const useFixVh = () => {
-  const setVh = useDebouncedUiCallback(() => {
+  const setVh = useDebouncedUiCallback((type) => {
+    console.log(type)
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   })
 
   if (screen?.orientation?.onchange) {
     screen.orientation.onchange = function () {
-      setVh()
+      setVh('orientation change')
     }
   }
 
   React.useEffect(() => {
-    setVh()
-    const listener = window.addEventListener('resize', setVh);
+    setVh('iniital')
+    const resized = () => setVh('resized')
+    const listener = window.addEventListener('resize', resized);
     return () => {
-      window.removeEventListener(listener, setVh)
+      window.removeEventListener(listener, resized)
     }
   })
 }
