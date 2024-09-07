346e7e1233fc77660f2958ee8ba2efdad2d2cd07
Brandon Mathis <brandon@imathis.com>
2024-09-07 17:38:07 -0500

    Improved language on format description

--- src/hooks/GameProvider.jsx
+++ src/hooks/GameProvider.jsx
@@ -7,7 +7,7 @@ const GameContext = React.createContext({})
 
 const getFormat = ({ card, game }) => {
   const onCourt = game.cards.filter(({ court }) => court === card.court).length
-  if (onCourt === 1) return 'rotating'
+  if (onCourt === 1) return 'you’re rotating'
   if (onCourt === 2) return 'playing singles'
   if (onCourt === 3) return 'with 3 players'
   return 'playing doubles'
