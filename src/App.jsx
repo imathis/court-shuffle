2aec6189b56f38bc53a44ac71c2499ad07df4ea0
Brandon Mathis <brandon@imathis.com>
2025-01-01 22:02:26 -0600

    Got dynamic import and asynchronously loaded cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
+import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { NewGame, Join, Play } from './game'
 import { GameProvider } from './hooks'
 import { useFixVh } from './hooks'
+import { preloadAllCards } from './assets/cards'
 
 const App = () => {
   useFixVh()
+  React.useEffect(() => {
+    preloadAllCards()
+  }, [])
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route index element={<NewGame />}/>
+          <Route index element={<NewGame />} />
           <Route path="new" element={<NewGame />} />
           <Route path="join" element={<Join />} />
           <Route path="game/*">
             <Route path=":game/*" element={<GameProvider />}>
-              <Route index element={<Play/>} />
+              <Route index element={<Play />} />
               <Route path="join" element={<Join />} />
             </Route>
           </Route>
